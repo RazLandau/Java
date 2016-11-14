4c466eba0ea9d7b6222ceca9bbeb40604c03f1c1
Raz Landau <landau.raz@gmail.com>
2016-11-14 22:04:51 +0200

    Add files via upload

---
+++ EX1/Assignment1.java
@@ -0,0 +1,18 @@
+
+public class Assignment1 {
+	public static void main (String[] arg){
+		int numerator	= Integer.parseInt(arg[0]);
+		int denominator	= Integer.parseInt(arg[1]);
+		if(denominator == 0){
+			System.out.println("Cannot perform zero division!");
+		}
+		else {
+			int division = numerator / denominator;
+			int remainder = numerator % denominator;
+			System.out.print("The division result is: " + String.valueOf(division));
+			if (remainder != 0){
+				System.out.println(", remainder is: " + String.valueOf(remainder));
+			}
+		}
+	}
+}
