cca197585c50a1bc93d20a4fe061a019b30ed97e
Raz Landau <raz@playbuzz.com>
2017-03-14 15:25:14 +0200

    Update find2.java

--- Miscellaneous/find2.java
+++ Miscellaneous/find2.java
@@ -13,7 +13,35 @@
 
 public class A2
 {
-    public static boolean find2 (int [][] mat, int x)
+ 
+    public static void main (String[] args)
+    {
+        {int[][] findInMat={
+        {-4,-2,5,9},
+        {2,5,12,13},
+        {13,20,25,25},
+        {22,24,49,57}};
+
+         System.out.println("Question 2\n=============");              
+        System.out.println("2) find method output is: " +  A2.find2(findInMat,48) + "  while the answer is: true\n");
+        
+        
+        /**
+        int [] a = new int [16];
+        int k=0;
+        for (int i=0; i<findInMat.length; i++)
+            for (int l=0; l<findInMat.length; l++)
+            {
+                a[k] = findInMat[i][l];
+                k++;
+            }
+                
+        for (int m=0; m<a.length; m++)
+            System.out.println(A2.find2(findInMat, a[m]));
+            */
+    }
+ 
+ public static boolean find2 (int [][] mat, int x)
     {
         int 
         firstRow = 0,
