363f23296bc386932554b0f4957bdf59257154b0
Raz Landau <raz@playbuzz.com>
2017-03-14 15:17:53 +0200

    Update isSink2.java

--- Miscellaneous/isSink2.java
+++ Miscellaneous/isSink2.java
@@ -13,6 +13,19 @@
 
 public class A1
 {
+     public static void main (String[] args){
+        
+        int[][] findSink={ //sink=1
+                {0,0,0,0,0},
+                {1,0,1,0,0},
+                {1,0,0,0,0},
+                {1,0,1,0,0},
+                {1,1,1,1,0}};
+
+        System.out.println("Question 1\n=============");        
+        System.out.println("1) is Sink method output is: " +  A1.isSink2(findSink) + "  while the answer is 2\n"); 
+    }
+ 
     public static int isSink2 (int [][] mat)
     {
         return isSink2 (mat, escapeArray(mat));
