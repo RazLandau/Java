3ac1bfd59fef1965febd1e4f56c458dd986751c5
Raz Landau <raz@playbuzz.com>
2017-03-14 14:20:23 +0200

    Update and rename EX2/Assignment02Q01Sec01.java to Miscellaneous/PrintEvenChars.java

--- Miscellaneous/PrintEvenChars.java
+++ Miscellaneous/PrintEvenChars.java
@@ -1,5 +1,7 @@
-
-public class Assignment02Q01Sec01 {
+/**
+* Prints all chars with even ascii value in given chars from CL
+*/
+public class PrintEvenChars {
 	public static void main (String[] arg){
 		for (String c : arg){
 			int ascii = (int)c.charAt(0);
